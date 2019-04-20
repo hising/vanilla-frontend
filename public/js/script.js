bb1be9205ba6b584f3aa32bdfd291014f37dc9d6
Mattias Hising <hising@gmail.com>
2019-04-20 14:16:13 +0200

    Fixed

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+/* If you need to wait for window onLoad */
+const onLoad = (event) => {
+    console.info("window.onload", event);
+};
+
+/* If you need to wait for DOM ready */
+const onDomReady = (event) => {
+  console.info("document.DOMContentLoaded", event);
+};
+
+/* If you do not need to wait for these events add code here */
+
+/* Just ugly handlers */
+document.addEventListener("DOMContentLoaded", onDomReady);
+window.addEventListener("load", onLoad);
+
